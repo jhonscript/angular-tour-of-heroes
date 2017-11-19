ed85e3a0b52a55daf71777cdf07e7c9ea2f61487
Jhon Guevara <jhonstile@gmail.com>
2017-11-19 14:44:20 -0500

    Add a new hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -57,6 +57,15 @@ export class HeroService {
       )
   }
 
+  addHero(hero: Hero): Observable<Hero>
+  {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`))
+        , catchError(this.handleError<Hero>('addHero'))
+      )
+  }
+
   private log(message: string)
   {
     this.messageService.add('HeroService ' + message);
